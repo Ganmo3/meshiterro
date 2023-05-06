524cee998a0ab0fbe9354493650ba75cc652be8f
Ganmo3 <131627788+Ganmo3@users.noreply.github.com>
2023-05-06 01:51:12 +0000

    [Finish] 4-19

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,6 +2,8 @@ class PostImage < ApplicationRecord
   
   has_one_attached :image
   belongs_to :user
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   def get_image
     if image.attached?
@@ -18,5 +20,10 @@ class PostImage < ApplicationRecord
     end
     image
   end
+    
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+    
     
 end
